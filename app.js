4d4e0d70daddc0bd9a8e111342b9190a68626d77
Dimitri2105 <karanthakur577@gmail.com>
2023-06-07 09:47:01 +0530

    Relations between Tables

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('647ff18401a04a0b18e3005b')
     .then(user => {
-      req.user = user;
+      req.user = new User (user.username , user.email , user.cart , user._id);
       next();
     })
     .catch(err => console.log(err));
-  next()
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -2,26 +2,52 @@ const mongoDB = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
-    (this.username = username), (this.email = email);
+  constructor(username, email , cart , id) {
+    this.username = username, 
+    this.email = email , 
+    this.cart = cart;   // it is an object like cart = { items :[]}
+    this._id = id
   }
 
   save() {
     const db = getDb();
     return db
-      .collections("Users")
+      .collection("Users")
       .insertOne(this)
       .then((user) => {
         console.log(user);
       })
       .catch((error) => console.log(error));
   }
+  addToCart(product){
+    const db = getDb();
+
+    const cartProductIndex = this.cart.items.findIndex( cp =>  cp.productId.toString() === product._id.toString())  //finding if the product index exist in cart or not 
+    
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items]                 //spreading the cart items
+
+    if (cartProductIndex >= 0){
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    }else{
+      updatedCartItems.push({ productId : new mongoDB.ObjectId(product._id) , quantity : newQuantity })
+    }
+    const updatedCart  = { items : updatedCartItems}   //creating product from scratch
+    return db.collection('Users')
+    .updateOne( {_id  : new mongoDB.ObjectId(this._id)} , { $set  : {cart : updatedCart}}) 
+    .then((item) => {
+      console.log(item);
+    })
+    .catch((error) => console.log(error));
+
+
+  }
   static findById(userId) {
     const db = getDb();
     return db
-      .collections("Users")
-      .find({ _id: new mongoDB.ObjectId(userId) })
-      .next()
+      .collection("Users")
+      .findOne({ _id: new mongoDB.ObjectId(userId) })
       .then((user) => {
         console.log(user);
         return user;
